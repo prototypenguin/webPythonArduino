2110459619e1f86d9270b2d5ade8c877e775ba0d
Sergio Pinho <sergioncp@gmail.com>
2016-05-16 12:47:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ render = web.template.render('templates/')
 urls = ('/', 'index')
 
 com = getdata.talk()
-paref = 15000
 
 class index:
 
     def GET(self):
         co = com.getstrval("A")
         
-        return render.index(paref, co)
+        return render.index(co)
         
 if __name__ == "__main__": 
     app = web.application(urls, globals())
