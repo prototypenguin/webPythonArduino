2e8eb3d091a4bf0b0367538903e97dfeb04f2905
Sergio Pinho <sergioncp@gmail.com>
2016-05-16 12:47:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,15 @@ import getdata
 render = web.template.render('templates/')
 urls = ('/', 'index')
 
-com = getdata.getval()
-paref = 15000 # page refresh delay in ms
-
-p1reslist = []
-p2reslist = []
+com = getdata.talk()
+paref = 15000
 
 class index:
 
     def GET(self):
-        co = com.testco("A")
-        diff = com.strval("B")
-        mlistes = getdata.listes()
-        p1res = mlistes.make(13,"C",p1reslist)
-        p2res = mlistes.make(13,"D",p2reslist)
-        pos = com.strval("E")
+        co = com.getstrval("A")
         
-        return render.index(paref, pos, co, diff, p1res, p2res)
+        return render.index(paref, co)
         
 if __name__ == "__main__": 
     app = web.application(urls, globals())
