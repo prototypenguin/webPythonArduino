c3062fa2651d541d2ef15bff99d14fcaa80a7eda
Sergio Pinho <sergioncp@gmail.com>
2016-05-16 12:33:36 +0200

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -1,22 +1,21 @@
 import serial
-ser = serial.Serial("/dev/ttyACM0",9600) # Connect to the Arduino
+ser = serial.Serial("/dev/ttyACM0",9600) 
 
 
 class listes(object):
     
-    def make(self, lmax, char, liste): 
+    def make(self, lmax, char, liste):
         """Takes the folliwing parametres and appends taken values to the list:
             lmax: max list length
             char: must be equal to the IF-'CHAR' on Arduino, CHAR been a letter from A to Z
             liste: predefined global list on main.py
         """
-      
+
         if len(liste) != lmax:
-        
-            ser.write(char)
-            val = int(ser.readline())
-            liste.append(val)
-            
+             ser.write(char)
+             val = int(ser.readline())
+             liste.append(val)
+                
         else:
         
             del liste[:]
@@ -26,26 +25,24 @@ class listes(object):
         
         return liste
 
-class getval(object):
+class talk(object):
     
-    def intval(self, char):
-        
+    def getintval(self, char):
+
         ser.write(char)
         val = int(ser.readline())
         
         return val
     
-    def strval(self, char):
-        
+    def getstrval(self, char):
+
         ser.write(char)
         val = ser.readline()
                 
         return val
-    
-    def testco(self, char):
-    
+        
+    def tell(self, char):
+
         ser.write(char)
-        val = ser.readline()
         
         return val
-
