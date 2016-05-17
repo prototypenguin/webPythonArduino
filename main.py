4f0213e129b560c851d5ba7fbcf8574cd5735c19
Sergio Pinho <sergioncp@gmail.com>
2016-05-17 10:33:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class index:
         co = com.get_str_val("A")
         val = com.get_int_val("B")
         
-        return render.index(co)
+        return render.index(co, val)
         
 if __name__ == "__main__": 
     app = web.application(urls, globals())
